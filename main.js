dc605a9d23cb6132ec067f396e137ff479cf39de
IgorCisowski <igorcisowski2098@gmail.com>
2022-11-14 17:26:06 +0100

    added a burger menu

--- main.js
+++ main.js
@@ -1,2 +1,19 @@
-// js
-//jsch
+const burger = document.querySelector(".burger");
+const menu = document.querySelector(".menu");
+const li = document.querySelectorAll("li");
+
+const toggle = () => {
+  menu.classList.toggle("menuActive");
+  burger.classList.toggle("active");
+};
+
+const close = () => {
+  menu.classList.remove("menuActive");
+  burger.classList.remove("active");
+};
+
+li.forEach((li) => {
+  li.addEventListener("click", close);
+});
+
+burger.addEventListener("click", toggle);
